3af1022c54208e5b418574077e8c8482d5dca084
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-10 11:12:22 -0800

    Updated

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - no input
+ * Description: print all the alphabets in lowercase except e and q
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *s = "abcdfghijklmnoprstuvwxyz";
+
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+	putchar('\0');
+	return (0);
+}
