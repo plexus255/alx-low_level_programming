eb5f3186c60ec1af6eb473e0633cea840e5766ba
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-10 07:12:18 -0800

    Updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - No input
+ * Description: This program print alphabets in lowcase using putchar func.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *s = "abcdefghijklmnopqrstuvwsyz";
+
+
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
