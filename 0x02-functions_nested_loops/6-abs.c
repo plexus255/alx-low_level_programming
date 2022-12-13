de5ab379747f8d51b52f906e2a2f03b5e59605cd
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-13 09:07:45 -0800

    task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,15 +4,14 @@
  * _abs - takes int n
  * Description: a function that computes the absolute value of an integer
  * @n: parameter format to work on
- * @int - return
- * return: int 
+ * Return: combuted absolute value
  */
 
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		return ((-1) * (n));
+		return ((-1) * n);
 	}
 	else if (n > 0)
 	{
