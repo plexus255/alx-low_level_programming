a4dd1dcee85fccc5c4161617fdc68497a633f7f7
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-13 14:59:46 -0800

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - takes no input
+ * Description: function that adds two integers and returns the result.
+ * @a: parameter format for the func.
+ * @b: parameter format
+ * Return: the computed addition of the two parameter formats
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+
