92555296b216626a6727f1c05a50f18623a40e01
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-14 04:44:54 -0800

    task

---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int r, w, d;
+for (r = 0; r < 10; r++)
+{
+	for (w = 0; w < 10; w++)
+	{
+		d =  r * w;
+		if (d > 9)
+		{
+			putchar('0' + d / 10);
+			putchar('0' + d % 10);
+		}
+		else
+		{
+			putchar('0' + d);
+		}
+		int m;
+
+		while ( m ==0 && w != 9)
+		{
+			putchar(',');
+			putchar(' ');
+			putchar(' ');
+			m++;
+		}
+		m = 0;
+
+	}
+	putchar('\n');
+}
+}
