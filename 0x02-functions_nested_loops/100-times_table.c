1fe2f3dbc5ea5ece87a62114da1af8744579a8ad
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-14 07:46:49 -0800

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,42 +10,44 @@
 void print_times_table(int n)
 {
 	int i, j, k, pro;
-	
-	k = n + 1;
-	for (i = 0; i < k; i++)
+
+	if (n <= 0 && n <= 14)
 	{
-		for (j = 0; j < k; j++)
+		for (i = 0; i < n + 1; i++)
 		{
-			pro = i * j;
-			if (pro > 9)
+			for (j = 0; j < n + 1; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + pro / 10);
-				_putchar('0' + pro % 10);
-			}
-			else if (pro > 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + pro / 100);
-				_putchar('0' + ((pro / 10) % 10));
-				_putchar('0' + pro % 10);
-			}
-			else
-			{
-				if (j != 0)
-				{
+				pro = i * j;
+				if (pro > 9)
+				{	
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
+					_putchar('0' + pro / 10);
+					_putchar('0' + pro % 10);
 				}
-				_putchar('0' + pro);
+				else if (pro > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + pro / 100);
+					_putchar('0' + ((pro / 10) % 10));
+					_putchar('0' + pro % 10);
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + pro);
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
 
