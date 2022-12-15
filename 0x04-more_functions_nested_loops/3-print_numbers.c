b1d55c10e99eb2dd688148eccfc5bf7246616114
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-15 05:59:06 -0800

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - take no input
+ * Description: function to print number 0 through 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - take no arg
+ * Description: working with putchar
+ * Return: 0
+ */
+
+void _putchar(char c)
+{
+	putchar(c);
+}
+
