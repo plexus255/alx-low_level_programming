1a5fcf6bf06340af88a2402cc50aee723ae92b29
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-15 17:06:53 -0800

    print days

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -18,5 +18,6 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif /* MAIN_H */
 
