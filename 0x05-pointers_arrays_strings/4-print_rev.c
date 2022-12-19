5c9fb6b7c7eeb1b2990bd0236e164fe9cfcf7052
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-19 12:09:15 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _print_rev - take pointers
+ * Description: function that prints a string, in reverse, followed by a new line.
+ * @s: A pointer parameter format
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
