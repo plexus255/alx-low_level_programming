1fdfa1be4b9209a9f5482ee30112ff65b3d3cc67
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-22 14:45:13 -0800

    task

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet - take address
+ * Description: a function that encodes a string into 1337.
+ * @str: pointer
+ * Return: pointer
+ */
+
+char *leet(char *ptr)
+{
+	char upstr[] = "AEOTL";
+	char lwstr[] = "aeotl";
+	char leetnum[] = "43071";
+
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (ptr[i] != '\0')
+	{
+		while (j < 5)
+		{
+			if (ptr[i] == upstr[j] || ptr[i] == lwstr[j])
+				ptr[i] = leetnum[j];
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (ptr);
+}
+
