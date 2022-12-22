9b44571bd91ea7cc8e2cf7ef8fd4151f9a6d61c1
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-22 02:48:56 -0800

    task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - take two addresses as input
+ * Description:  a function that copy a strings up to n bytes
+ * @dest: pointer to first argument
+ * @src: pointer to second arg.
+ * @n: src length
+ * Return: first pointer aka dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < n)
+	{
+		dest[j] = src[i];
+		if (src[i] == '\0')
+			break;
+		j++;
+		i++;
+	}
+	return (dest);
+}
+
+
