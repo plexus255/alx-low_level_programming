aa450400be48e4631035937e0a557f387df69833
Abdulgithub0 <belloabdulsamad26@gmail.com>
2022-12-22 09:09:38 -0800

    task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _string_toupper - take one address as input
+ * Description:  a function that convert strings to uppercase
+ * @str: pointer agr
+ * Return:  pointer
+ */
+
+char *string_toupper(char *str)
+{
+	int i, j;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		if (str[j] >= 'a' && str[j] <= 'z')
+		{
+			str[j] = str[j] - 32;
+		}
+		j++;
+	}
+	return (str);
+}
+
+
