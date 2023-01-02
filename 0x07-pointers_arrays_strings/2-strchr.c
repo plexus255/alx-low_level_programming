ef1d07f4ff6c2bc313228b3bdf791610af22e293
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-02 07:38:01 -0800

    invoke c program for strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: pointer to the address that will be used locate the intended char
+ * @c: parameter variable for the char that will be look for
+ * Return: address of the char if found or NULL if not
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = -1;
+	do
+	{
+		i++;
+	}while (*(s + i) != c);
+	if (*(s + i) == c)
+	{
+		return (s + i);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
