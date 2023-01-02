f104e02c675e40dd452892e1a195786f09adfaa2
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-02 06:43:51 -0800

    invoke c program for memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * memset - a function that fills memory with a constant byte.
+ * @s: pointer to the buffer address
+ * @b: character to fill into the buffer
+ * @n: sizeof buffer
+ * Return: address a.k.a pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
+
