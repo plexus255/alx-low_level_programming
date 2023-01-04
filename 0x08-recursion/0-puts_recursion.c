8638f6f99aa28bbcc3c5b301465a263567d2d9a4
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-04 06:25:43 -0800

    print with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -function that simulate 'puts' with recurs. logic
+ * @s: pointer to the string address.
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
