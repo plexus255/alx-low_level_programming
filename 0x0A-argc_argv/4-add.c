49b88ca0e517ab7a1caa5cfb0a6c7b602821353b
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-06 04:24:04 -0800

    I like this task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - a program that add positive number
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	j = 0;
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if ((atoi(argv[i]) != 0))
+			{
+				j += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", j);
+		return (0);
+	}
+	printf("%d\n", j);
+	return (0);
+}
---
+++ 0x0A-argc_argv/test.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(void)
+{
+int i;
+char *c = "1000";
+i = atoi(c);
+printf("%d\n", i);
+}
