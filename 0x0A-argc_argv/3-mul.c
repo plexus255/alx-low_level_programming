8d9228f4a1106f5bf47f9cca36bef3abb2f37598
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-06 03:14:09 -0800

    perform multiplication on the pass in cm line int argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - a program that multiplies two numbers type on cm line
+ * argc - argument count
+ * argv - argument vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j;
+
+	if (argc == 3)
+	{
+		i = atoi(*(argv + 1));
+		j = atoi(*(argv + 2));
+		i = i * j;
+		printf("%d\n", i);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
