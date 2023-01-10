9cd3ef351e648d189b02163857d4ee1750ac1cc4
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-10 03:43:49 -0800

    task 2, still on malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - this function simulate func. 'strup' in C stdlib
+ * @str: address of string
+ * Return: address
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+
+	int i;
+
+	s = malloc(sizeof(str));
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (i < (sizeof(str)))
+	{
+		s[i] = str[i];
+		i++;
+	}
+	return (s);
+}
+
