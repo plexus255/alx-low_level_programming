759fff9de1bdad6d50e99a7fa6f744c2e90ab979
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-16 03:41:02 -0800

    structure type for dog data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type for declaring struct variable for dog
+ * @name: data member for describing the dog's name
+ * @age: data member for describing the age of the dog
+ * @owner: data member for describing the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
